ffe77bfdaf1f857a483c7eb0c7769fcfc59c3214
egeoral <egeoral@gmail.com>
2026-04-12 23:08:55 +0300

    Settings popup wip

--- Assets/Scripts/UI/Popup/SettingsPopup.cs
+++ Assets/Scripts/UI/Popup/SettingsPopup.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,24 +8,45 @@ namespace UI.Popup
     public class SettingsPopup : BasePopup
     {
         [Header("Buttons")]
-        // [SerializeField] private Button closeButton;
+        [SerializeField] private Button closeButton;
+        [SerializeField] private Button soundToggleButton;
+        // [SerializeField] private Button musicToggleButton;
+        // [SerializeField] private Button vibrationToggleButton;
+        // [SerializeField] private Button notificationToggleButton;
 
-        // [Header("Toggles")]
-        // [SerializeField] private Toggle soundToggle;
-        // [SerializeField] private Toggle musicToggle;
-        // [SerializeField] private Toggle vibrationToggle;
-        // [SerializeField] private Toggle notificationToggle;
+        [Header("Sliders")]
+        [SerializeField] private Slider soundToggle;
+        // [SerializeField] private Slider musicToggle;
+        // [SerializeField] private Slider vibrationToggle;
+        // [SerializeField] private Slider notificationToggle;
+
+        [Header("Toggle Settings")]
+        [SerializeField] private float toggleDuration = 0.2f;
 
         public override string PopupName => PopupKeys.Settings;
 
         private void OnEnable()
         {
-            // closeButton.onClick.AddListener(OnCloseButtonPressed);
+            closeButton.onClick.AddListener(OnCloseButtonPressed);
+            soundToggleButton.onClick.AddListener(() => ToggleSlider(soundToggle));
+            // musicToggleButton.onClick.AddListener(() => ToggleSlider(musicToggle));
+            // vibrationToggleButton.onClick.AddListener(() => ToggleSlider(vibrationToggle));
+            // notificationToggleButton.onClick.AddListener(() => ToggleSlider(notificationToggle));
         }
 
         private void OnDisable()
         {
-            // closeButton.onClick.RemoveListener(OnCloseButtonPressed);
+            closeButton.onClick.RemoveListener(OnCloseButtonPressed);
+            soundToggleButton.onClick.RemoveAllListeners();
+            // musicToggleButton.onClick.RemoveAllListeners();
+            // vibrationToggleButton.onClick.RemoveAllListeners();
+            // notificationToggleButton.onClick.RemoveAllListeners();
+        }
+
+        private void ToggleSlider(Slider slider)
+        {
+            float target = slider.value < 0.5f ? 1f : 0f;
+            slider.DOValue(target, toggleDuration).SetEase(Ease.OutQuad).SetUpdate(true);
         }
 
         private void OnCloseButtonPressed()
